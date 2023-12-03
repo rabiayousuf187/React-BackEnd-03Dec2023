301c7b9e8afa41875234c2d971adddf7e005bccc
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-12-03 11:39:07 +0500

    rename file and coded to read file

---
+++ module/file.js
@@ -0,0 +1,18 @@
+// var fs = require('fs');
+// Common use for the File System module:
+
+// Read files
+// Create files
+// Update files
+// Delete files
+// Rename files
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
